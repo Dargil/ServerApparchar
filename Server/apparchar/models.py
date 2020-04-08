f821e9b0a3a975a146e8b6e8720bdf3412b66ae0
Jefferson <jefferson.marin@correo.usa.edu.co>
2020-04-07 20:55:14 -0500

    changed od models user

--- Server/apparchar/models.py
+++ Server/apparchar/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 #este van a ser mis modelos
 
 class User(AbstractUser):
-    age=models.IntegerField(blank=True,null=True,unique=True)
-    descripcion=models.CharField(max_length=150,blank=True,null=True)
 
+    NIT=models.CharField(max_length=150,blank=True,null=True)
+    direccion=models.CharField(max_length=150,blank=True,null=True)
+    telefono=models.CharField(max_length=150,blank=True,null=True)
     USERNAME_FIELD='username'
-    REQUIRED_FIELDS=['age']
--- Server/apparchar/serializers.py
+++ Server/apparchar/serializers.py
@@ -5,7 +5,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
             model = User
-            fields = ['email', 'username', 'password', 'first_name','last_name']
+            fields = ['NIT','first_name','last_name','email','direccion','telefono', 'username', 'password', ]
             extra_kwargs = {'password': {'write_only': True}}
 
 
