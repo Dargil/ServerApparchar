2e8a27a60aef022e741c11626d267759a9fbadc6
juanpape24 <juanpape24@gmail.com>
2020-04-19 10:27:28 -0500

    conexion con s3

--- Server/apparchar/urls.py
+++ Server/apparchar/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .viewsets import clienteReq
+from .viewsets import clienteReq,fotoPerfilUsuario
 
 urlpatterns = [
     path('cliente/', clienteReq),
+    path('cliente/uploadFoto',fotoPerfilUsuario),
 ]
--- Server/apparchar/viewsets.py
+++ Server/apparchar/viewsets.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response
 from .models import cliente
 from .serializer import ClienteSerializer
 import json
+import boto3
+from PIL import Image
+from io import BytesIO
+import base64
 
 
 @api_view(['GET', 'POST'])
@@ -18,15 +22,8 @@ def clienteReq(request):
         try:
             usuarioxd = cliente.objects.get(usuario=user, contrasenia=password)
             return Response({'validate': True}, status=status.HTTP_200_OK)
-        except :
+        except:
             return Response({'validate': False}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-
-
 
     elif request.method == 'POST':
         serializer = ClienteSerializer(data=request.data)
@@ -34,3 +31,22 @@ def clienteReq(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST'])
+def fotoPerfilUsuario(request):
+    if request.method=='POST':
+        photo=request.data
+        ruta = request.GET['ruta']
+        #try:
+         #   client=boto3.client('s3')
+          #  client.upload_fileobj(photo, "apparchar", ruta)   
+           # return Response(status=status.HTTP_200_OK)
+        #except:
+         #   return Response({'validate': False}, status=status.HTTP_400_BAD_REQUEST)
+
+        client=boto3.client('s3')
+        client.upload_fileobj(photo.get('myfile'), "apparchar", ruta)
+        return Response({'validate': True}, status=status.HTTP_200_OK)
+
+
+
